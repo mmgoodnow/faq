4191843dca33e337d30eb8cc01f3ab6f973b54e3
Michael Goodnow <mmgoodnow@gmail.com>
2020-08-08 02:03:26 -0400

    do a quick demo of collapsing faqs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const QUESTIONS = [
+  {
+    question: "what is life1",
+    answer: "stuff1",
+  },
+  {
+    question: "what is life2",
+    answer: "stuff2",
+  },
+  {
+    question: "what is life3",
+    answer: "stuff3",
+  },
+];
+
+class FaqItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ open: !this.state.open });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="whatever" onClick={this.handleClick}>
+          {this.props.question}
+          <img
+            alt=""
+            width="10px"
+            className={`chevron ${this.state.open ? "down" : "right"}`}
+            src="https://image.freepik.com/free-icon/chevron-left_318-10769.jpg"
+          />
+        </div>
+        {this.state.open && <div>{this.props.answer}</div>}
+      </React.Fragment>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {QUESTIONS.map((q) => (
+          <FaqItem question={q.question} answer={q.answer} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
